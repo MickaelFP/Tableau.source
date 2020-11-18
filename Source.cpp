48484608e6ff94b14451ab917c1f86efce969cd5
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-18 11:49:14 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//tabelau
 int findInt(int tab[10], int toSearch) {
     for (int i = 0; i < 10; i++) {
         if (tab[i] == toSearch) {
@@ -22,3 +23,24 @@ int main() {
                 
     return 0;
 }
+
+//naviguer dans le tableau -> cas
+    int a = 5;
+    
+    switch (a) {
+    case 0:
+        cout << " je suis le cas 0 " << endl;
+        break;
+    case 1:
+        cout << " je suis le cas 1 " << endl;
+        break;
+    case 2:
+        cout << " je suis le cas 2 " << endl;
+        break;
+    case 3:
+        cout << " je suis le cas 3 " << endl;
+        break;
+    default:
+        cout << 'je suis le cas default" << andl;
+        break;
+    }
